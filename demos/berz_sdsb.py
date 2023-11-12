8f9aef6bf34c6f380b67ed4c07e50caf8271954f
zgbkdlm <zgbkdlm@gmail.com>
2023-11-12 11:08:57 +0100

    try bverz

--- demos/berz_sdsb.py
+++ demos/berz_sdsb.py
@@ -10,33 +10,36 @@ import optax
 import matplotlib.pyplot as plt
 from flax import linen as nn
 from fbs.nn.utils import make_nn_with_time
-from functools import partial
 
 parser = argparse.ArgumentParser(description='Test supervised DSB on Berzelius.')
 parser.add_argument('--train', action='store_true', help='Whether to train or test.')
 args = parser.parse_args()
 
 # General configs
-nsamples = 10000
+nsamples = 400
 jax.config.update("jax_enable_x64", True)
+nn_float = jnp.float64
 nn_param_init = nn.initializers.xavier_normal()
 key = jax.random.PRNGKey(666)
 
 dt = 0.01
-nsteps = 200
+nsteps = 500
 T = nsteps * dt
 ts = jnp.linspace(dt, T, nsteps)
+ts_training = ts[:-1]
 
 
 # Neural network construction
 class MLP(nn.Module):
     @nn.compact
     def __call__(self, x):
-        x = nn.Dense(features=100, param_dtype=jnp.float64, kernel_init=nn_param_init)(x)
-        x = nn.gelu(x)
-        x = nn.Dense(features=20, param_dtype=jnp.float64, kernel_init=nn_param_init)(x)
-        x = nn.gelu(x)
-        x = nn.Dense(features=2, param_dtype=jnp.float64, kernel_init=nn_param_init)(x)
+        x = nn.Dense(features=100, param_dtype=nn_float, kernel_init=nn_param_init)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=50, param_dtype=nn_float, kernel_init=nn_param_init)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=20, param_dtype=nn_float, kernel_init=nn_param_init)(x)
+        x = nn.relu(x)
+        x = nn.Dense(features=2, param_dtype=nn_float, kernel_init=nn_param_init)(x)
         return jnp.squeeze(x)
 
 
@@ -44,28 +47,15 @@ mlp = MLP()
 key, subkey = jax.random.split(key)
 init_param, _, nn_eval = make_nn_with_time(mlp, dim_in=2, batch_size=10, key=subkey)
 
-# Draw samples on the two sides
-np.random.seed(999)
-
-
-@partial(jax.vmap, in_axes=[0])
-def f1(z):
-    """x = f1(z)"""
-    u = jnp.sin(0.5 * z)
-    return jnp.array([(u[0] + u[1]) / 2, u[0] * u[1]])
-
-
-@partial(jax.vmap, in_axes=[0])
-def f2(x):
-    """y = f2(x)"""
-    u = jnp.tanh(2 * x)
-    return jnp.array([u[0] * u[1], (u[0] + u[1]) / 2])
-
-
+# Draw training samples
 key, subkey = jax.random.split(key)
-zs = jax.random.normal(subkey, (nsamples, 2))
-xs = f1(zs)
-ys = f2(xs)
+keys = jax.random.split(subkey, num=4)
+g1 = jnp.array([3, 3]) + 0.2 * jax.random.normal(keys[0], (int(nsamples / 4), 2))
+g2 = jnp.array([-3, 3]) + 0.2 * jax.random.normal(keys[1], (int(nsamples / 4), 2))
+g3 = jnp.array([-3, -3]) + 0.2 * jax.random.normal(keys[2], (int(nsamples / 4), 2))
+g4 = jnp.array([3, -3]) + 0.2 * jax.random.normal(keys[3], (int(nsamples / 4), 2))
+xs = jnp.concatenate([g1, g2, g3, g4], axis=0)
+ys = -5 / xs
 
 
 # Define the noising forward reference
@@ -88,60 +78,71 @@ def simulate_bridge_forward(x0, xT, _key):
     return jnp.concatenate([traj, xT.reshape(1, -1)])
 
 
-scales = (T - ts) * ts / (T * dt) + 1e-8
+scales = (T - ts) * ts / (T * dt) + 0
 
 
 def cond_pdf_t_0(xt, t, xT, x0):
-    mt, vt = x0 + t / T * (xT - x0), (T - t) * t / T + 1e-8
+    mt, vt = x0 + t / T * (xT - x0), (T - t) * t / T
     return jnp.sum(jax.scipy.stats.norm.logpdf(xt, mt, jnp.sqrt(vt)), axis=-1)
 
 
-@partial(jax.vmap, in_axes=[0, None, 0, 0])
-@partial(jax.vmap, in_axes=[0, 0, None, None])
 def cond_score_t_0(xt, t, xT, x0):
     return jax.grad(cond_pdf_t_0)(xt, t, xT, x0)
 
 
-if args.train:
-    def loss_fn(_param, _key, overfit=False):
-        if overfit:
-            _xs = xs
-            _ys = ys
-            _keys = jax.random.split(_key, num=nsamples)
-        else:
-            _key, _subkey = jax.random.split(_key)
-            _zs = jax.random.normal(_subkey, (nsamples, 2))
-            _xs = f1(zs)
-            _ys = f2(xs)
-            _keys = jax.random.split(_subkey, num=nsamples)
-        forward_paths = jax.vmap(simulate_bridge_forward, in_axes=[0, 0, 0])(_xs, _ys, _keys)  # (nsamples, nsteps, 2)
-        errs = (jax.vmap(jax.vmap(nn_eval,
-                                  in_axes=[0, 0, None]),
-                         in_axes=[0, None, None])(forward_paths, ts, _param) -
-                cond_score_t_0(forward_paths, ts, _ys, _xs)) ** 2  # (nsamples, nsteps, 2)
-        return jnp.sum(jnp.mean(errs, 0))
+def compute_errs(x0, xT, _param, _key):
+    def scan_body(carry, elem):
+        x, err = carry
+        t, dw = elem
+
+        x = x + drift(x, xT, t) * dt + dw
+        err = err + jnp.sum((nn_eval(x, t, _param) - cond_score_t_0(x, t, xT, x0)) ** 2)
+        return (x, err), None
+
+    _, _subkey = jax.random.split(_key)
+    rnds = math.sqrt(dt) * jax.random.normal(_subkey, (nsteps, 2))
+    return jax.lax.scan(scan_body, (x0, 0.), (ts[:-1], rnds[:-1]))[0][1]
 
 
-    @jax.jit
-    def opt_step_kernel(_param, _opt_state, _key):
-        _loss, grad = jax.value_and_grad(loss_fn)(_param, _key)
-        updates, _opt_state = optimiser.update(grad, _opt_state, _param)
-        _param = optax.apply_updates(_param, updates)
-        return _param, _opt_state, _loss
+def loss_fn(_param, _key, overfit=False):
+    if overfit:
+        _xs = xs
+        _ys = ys
+    else:
+        _key, _subkey = jax.random.split(_key)
+        _keys = jax.random.split(_subkey, num=4)
+        _g1 = jnp.array([3, 3]) + 0.2 * jax.random.normal(_keys[0], (int(nsamples / 4), 2))
+        _g2 = jnp.array([-3, 3]) + 0.2 * jax.random.normal(_keys[1], (int(nsamples / 4), 2))
+        _g3 = jnp.array([-3, -3]) + 0.2 * jax.random.normal(_keys[2], (int(nsamples / 4), 2))
+        _g4 = jnp.array([3, -3]) + 0.2 * jax.random.normal(_keys[3], (int(nsamples / 4), 2))
+        _xs = jnp.concatenate([_g1, _g2, _g3, _g4], axis=0)
+        _ys = -5 / xs
+    _keys = jax.random.split(_key, num=nsamples)
+    errs = jax.vmap(compute_errs, in_axes=[0, 0, None, 0])(_xs, _ys, _param, _keys)
+    return jnp.mean(errs)
 
 
-    optimiser = optax.adam(learning_rate=1e-2)
-    opt_state = optimiser.init(init_param)
-    param = init_param
+@jax.jit
+def opt_step_kernel(_param, _opt_state, _key):
+    _loss, grad = jax.value_and_grad(loss_fn)(_param, _key)
+    updates, _opt_state = optimiser.update(grad, _opt_state, _param)
+    _param = optax.apply_updates(_param, updates)
+    return _param, _opt_state, _loss
 
-    for i in range(10000):
+
+optimiser = optax.adam(learning_rate=1e-3)
+opt_state = optimiser.init(init_param)
+param = init_param
+
+if args.training:
+    for i in range(100000):
         key, subkey = jax.random.split(key)
         param, opt_state, loss = opt_step_kernel(param, opt_state, subkey)
         print(f'i: {i}, loss: {loss}')
 
-    np.save('./param_sdsb', param)
+    np.save('param_sdsb', param)
 else:
-    param = np.load('./param_sdsb.npy')
+    param = np.load('param_sdsb.npy')
 
     # Backward sampling
     def simulate_backward(xT, _key):
@@ -154,29 +155,37 @@ else:
 
         _, _subkey = jax.random.split(_key)
         dws = jnp.sqrt(dt) * jax.random.normal(_subkey, (nsteps, 2))
-        return jax.lax.scan(scan_body, xT, (ts, dws))[1]
+        return jax.lax.scan(scan_body, xT, (ts[:-1], dws[:-1]))[1]
 
 
     key, subkey = jax.random.split(key)
     keys = jax.random.split(subkey, num=nsamples)
     backward_traj = jax.vmap(simulate_backward, in_axes=[0, 0])(ys, keys)
 
-    # Show the original data
-    fig, axes = plt.subplots(ncols=2, figsize=(12, 5))
-    axes[0].scatter(ys[:, 0], ys[:, 1], s=1)
-    axes[1].scatter(xs[:, 0], xs[:, 1], s=1)
+    fig, axes = plt.subplots()
+    axes.scatter(ys[:, 0], ys[:, 1], s=1)
+    axes.scatter(xs[:, 0], xs[:, 1], s=1)
+    # for ax in axes:
+    #     ax.set_xlim([-4, 4])
+    #     ax.set_ylim([-1, 1])
+    plt.show()
 
-    # Show the generated data
-    axes[1].scatter(backward_traj[:, -1, 0], backward_traj[:, -1, 1], s=1, c='tab:red', alpha=0.3)
+    fig, axes = plt.subplots()
+    axes.scatter(ys[:, 0], ys[:, 1], s=1)
+    axes.scatter(backward_traj[:, -1, 0], backward_traj[:, -1, 1], s=1)
+    plt.tight_layout(pad=0.1)
 
     # Mark
-    idx = 10
+    idx = 2
     key, subkey = jax.random.split(key)
-    keys = jax.random.split(subkey, num=nsamples)
+    keys = jax.random.split(subkey, num=10)
     mark_y = ys[idx]
-    mark_xs = jax.vmap(simulate_backward, in_axes=[None, 0])(mark_y, keys)[:, -1]
-    axes[0].scatter(mark_y[0], mark_y[1], s=10, c='tab:red')
-    axes[1].scatter(mark_xs[:, 0], mark_xs[:, 1], s=1, c='black')
-    axes[1].scatter(xs[idx, 0], xs[idx, 1], s=10, c='tab:red', marker='x')
-    plt.tight_layout(pad=0.1)
-    plt.show()
+    mark_x = xs[idx]
+    mark_xs = jax.vmap(simulate_backward, in_axes=[None, 0])(mark_y, keys)[:, -1, :]
+    axes.scatter(mark_y[0], mark_y[1], s=10, c='tab:red')
+    axes.scatter(mark_x[0], mark_x[1], s=10, c='tab:red')
+    axes.scatter(mark_xs[:, 0], mark_xs[:, 1], s=10, c='black', alpha=0.5, marker='x')
+
+    # for ax in axes:
+    #     ax.set_xlim([-4, 4])
+    #     ax.set_ylim([-1, 1])
---
+++ demos/berz_sdsb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A Berzelius-2023-194
+#SBATCH --gpus=1
+#SBATCH -t 15:00:00
+
+source ~/.bashrc
+export XLA_PYTHON_CLIENT_PREALLOCATE=true
+
+cd $WRKDIR/fbs
+source ./venv/bin/activate
+
+cd ./experiments
+
+nvidia-smi
+python berz_sdsb.py --training
+
