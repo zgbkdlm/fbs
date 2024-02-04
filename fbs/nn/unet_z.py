0bd7b12db4fea0e70bbad28093a8b90580943698
Zheng Zhao <zgbkdlm@gmail.com>
2024-02-04 11:29:30 +0100

    tes

--- fbs/nn/unet_z.py
+++ fbs/nn/unet_z.py
@@ -114,7 +114,7 @@ class ResBlock(nn.Module):
 
 class MNISTUNet(nn.Module):
     dt: float
-    features: Sequence[int] = (16, 32, 64)
+    features: Sequence[int] = (32, 64, 128)
     nchannels: int = 1
     upsampling_method: str = 'pixel_shuffle'
 
