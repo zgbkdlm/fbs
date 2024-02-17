aef5b32d2399ada8b9ba462ce8c5f848414892a4
zgbkdlm <zgbkdlm@gmail.com>
2024-02-17 20:16:07 +0100

    try a thinner nn

--- fbs/nn/unet_z.py
+++ fbs/nn/unet_z.py
@@ -136,7 +136,7 @@ class MNISTUNet(nn.Module):
         up_layers = []
         for i, feature in enumerate(self.features):
             x = ResBlock(feature)(x, time_emb)
-            x = Attention()(x)
+            # x = Attention()(x)
             up_layers.append(x)
             if i < len(self.features) - 1:
                 x = nn.Conv(feature, kernel_size=(3, 3), strides=(2, 2))(x)
@@ -148,7 +148,7 @@ class MNISTUNet(nn.Module):
         # Up pass
         for i in reversed(range(len(self.features))):
             x = ResBlock(self.features[i])(x, time_emb)
-            x = Attention()(x)
+            # x = Attention()(x)
             x = jnp.concatenate([up_layers[i], x], -1)
             x = ResBlock(self.features[i])(x, time_emb)
             if i > 0:
