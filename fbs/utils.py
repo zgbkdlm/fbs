de623b03700ef410cdaa173a9056bcfc8cb50c69
zgbkdlm <zgbkdlm@gmail.com>
2024-03-24 13:38:30 +0100

    add toy

--- fbs/utils.py
+++ fbs/utils.py
@@ -15,3 +15,11 @@ def discretise_lti_sde(A: JArray, gamma: JArray, dt: FloatScalar) -> Tuple[JArra
     AB = jax.scipy.linalg.expm(phi * dt) @ jnp.vstack([jnp.zeros_like(A), jnp.eye(d)])
     cov = AB[0:d, :] @ F.T
     return F, cov
+
+
+def bures_dist(m1, cov1, m2, cov2):
+    """The Wasserstein distance between two Gaussians.
+    """
+    chol = jnp.linalg.cholesky(cov1)
+    A = cov1 + cov2 - 2 * jnp.linalg.cholesky(chol @ cov2 @ chol)
+    return jnp.sum((m1 - m2) ** 2) + jnp.trace(A)
