02da02658704cc9e1b9ec60c1d491d3512595123
Zheng Zhao <zgbkdlm@gmail.com>
2024-03-25 14:27:12 +0100

    run local

--- experiments/run_toy_local.sh
+++ experiments/run_toy_local.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-for i in $(seq 0 99);
-do
-    python toy/gp_filter.py --id=$i --d=100 --nsamples=10000 --nparticles=10
-done
-
-for i in $(seq 0 99);
-do
-    python toy/gp_gibbs.py --id=$i --d=100 --nsamples=10000 --nparticles=10 --explicit_backward
-done
-
-for i in $(seq 0 99);
-do
-    python toy/gp_pmcmc.py --id=$i --d=100 --nsamples=10000 --nparticles=10 --delta=0.01
-done
+if [[ "$1" == "filter" ]]
+then
+    for i in $(seq 0 49);
+    do
+        python toy/gp_filter.py --id=$i --d=100 --nsamples=10000 --nparticles=10
+    done
+elif [[ "$1" == "gibbs" ]]
+then
+    for i in $(seq 0 49);
+    do
+        python toy/gp_gibbs.py --id=$i --d=100 --nsamples=10000 --nparticles=10 --explicit_backward
+    done
+elif [[ "$1" == "pmcmc" ]]
+then
+    for i in $(seq 0 49);
+    do
+        python toy/gp_pmcmc.py --id=$i --d=100 --nsamples=10000 --nparticles=10 --delta=0.01
+    done
+else
+    echo "What do you mean?"
+fi
