0676314a9cb1c85f7017dce49b3551773cb2c34f
Zheng Zhao <zgbkdlm@gmail.com>
2024-04-05 12:38:41 +0200

    add plots

---
+++ experiments/tabulators/plot_autocorrs.py
@@ -0,0 +1,51 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+import numpyro as npr
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': "serif",
+    'text.latex.preamble': r'\usepackage{amsmath,amsfonts}',
+    'font.size': 16})
+
+fig, ax = plt.subplots()
+
+jax.config.update("jax_enable_x64", True)
+
+sde = 'const'
+nparticles = 10
+
+methods = [f'gibbs-eb-{sde}-{nparticles}',
+           f'pmcmc-0.005-{sde}-{nparticles}']
+method_labels = ['', '', '']
+max_mcs = 100
+max_lags = 100
+q = 0.95
+
+autocorrs = np.zeros((max_mcs, max_lags))
+
+for method, method_label in zip(methods, method_labels):
+    for mc_id in range(max_mcs):
+        # Load
+        filename = f'./toy/results/{method}-{mc_id}.npz'
+        results = np.load(filename)
+        samples = results['samples']
+
+        acs = jnp.mean(npr.diagnostics.autocorrelation(samples, axis=1)[:, max_lags, :], axis=0)
+        autocorrs[mc_id] = np.quantile(acs, q=q, axis=-1)
+
+    autocorr_mean = jnp.mean(autocorrs, axis=0)
+    autocorr_std = jnp.std(autocorrs, axis=0)
+    ax.plot(jnp.arange(max_lags),
+            autocorr_mean, label=method_label)
+    ax.fill_between(jnp.arange(max_lags),
+                    autocorr_mean - 1.96 * autocorr_std,
+                    autocorr_mean + 1.96 * autocorr_std,
+                    alpha=0.3, color='black', edgecolor='none')
+
+ax.grid(linestyle='--', alpha=0.3, which='both')
+plt.tight_layout(pad=0.1)
+plt.legend()
+plt.show()
--- experiments/tabulators/tabulate_toy.py
+++ experiments/tabulators/tabulate_toy.py
@@ -6,56 +6,66 @@ Run this script under the folder `./experiments`
 import jax
 import jax.numpy as jnp
 import numpy as np
-import numpyro as npr
 import scipy
-import matplotlib.pyplot as plt
 from fbs.utils import kl
+from functools import partial
 
 jax.config.update("jax_enable_x64", True)
 
-methods = ['filter', 'gibbs-eb', 'pmcmc-0.01', 'twisted']
-method_label = ['', '', '']
-max_mcs = 50
-max_lags = 100
+sde = 'const'
+nparticles = 10
+
+methods = [f'filter-{sde}-{nparticles}',
+           f'gibbs-eb-{sde}-{nparticles}',
+           f'pmcmc-0.005-{sde}-{nparticles}',
+           f'twisted-{sde}-{nparticles}']
+max_mcs = 100
 q = 0.95
 
+errs_m = np.zeros((max_mcs,))
+errs_vars = np.zeros((max_mcs,))  # marginal variances
 errs_kl = np.zeros((max_mcs,))
 errs_skew = np.zeros((max_mcs,))
 errs_kurt = np.zeros((max_mcs,))
-autocorrs = np.zeros((max_mcs, max_lags))
 
+# Tabulate
 for method in methods:
     for mc_id in range(max_mcs):
+
         # Load
         filename = f'./toy/results/{method}-{mc_id}.npz'
         results = np.load(filename)
         samples, gp_mean, gp_cov = results['samples'], results['gp_mean'], results['gp_cov']
 
-        # Compute errors
-        approx_mean = jnp.mean(samples, axis=0)
-        approx_cov = jnp.cov(samples, rowvar=False)
+        if any(['gibbs', 'pmcmc' in method]):
+            approx_means = jax.vmap(partial(jnp.mean, axis=0))(samples)
+            approx_covs = jax.vmap(partial(jnp.cov, rowvar=False))(samples)
 
-        err_kl = kl(gp_mean, gp_cov, approx_mean, approx_cov)
-        err_skew = jnp.mean(jnp.abs(scipy.stats.skew(samples, axis=0)))
-        err_kurt = jnp.mean(jnp.abs(scipy.stats.kurtosis(samples, axis=0, fisher=True)))
+            err_m = jnp.mean(jnp.abs(approx_means - gp_mean[None, :]))
+            err_var = jnp.mean(jnp.abs(jnp.diag(approx_covs - gp_cov[None, :, :])))
+            err_kl = jnp.mean(jax.vmap(kl, in_axes=[None, None, 0, 0])(gp_mean, gp_cov, approx_means, approx_covs))
+            err_skew = jnp.mean(jnp.abs(scipy.stats.skew(samples, axis=1)))
+            err_kurt = jnp.mean(jnp.abs(scipy.stats.kurtosis(samples, axis=1, fisher=True)))
+        else:
+            approx_mean = jnp.mean(samples, axis=0)
+            approx_cov = jnp.cov(samples, rowvar=False)
 
+            err_m = jnp.mean(jnp.abs(approx_mean - gp_mean))
+            err_var = jnp.mean(jnp.abs(jnp.diag(approx_cov) - jnp.diag(gp_cov)))
+            err_kl = kl(gp_mean, gp_cov, approx_mean, approx_cov)
+            err_skew = jnp.mean(jnp.abs(scipy.stats.skew(samples, axis=0)))
+            err_kurt = jnp.mean(jnp.abs(scipy.stats.kurtosis(samples, axis=0, fisher=True)))
+
+        errs_m[mc_id] = err_m
+        errs_vars[mc_id] = err_var
         errs_kl[mc_id] = err_kl
         errs_skew[mc_id] = err_skew
         errs_kurt[mc_id] = err_kurt
 
-        # Autocorrelation
-        if method != 'filter':
-            autocorrs[mc_id] = np.quantile(npr.diagnostics.autocorrelation(samples, axis=0)[:max_lags],
-                                           q=q, axis=1)
-
-    # Tabulate
     print(f'Method {method} | '
+          f'Mean {jnp.mean(errs_m):.4f} {jnp.std(errs_m):.4f} | '
+          f'Var {jnp.mean(errs_vars):.4f} {jnp.std(errs_vars):.4f} | '
           f'KL | {jnp.mean(errs_kl):.4f} {jnp.std(errs_kl):.4f} | '
           f'Skew | {jnp.mean(errs_skew):.4f} {jnp.std(errs_skew):.4f} | '
           f'Kurt | {jnp.mean(errs_kurt):.4f} {jnp.std(errs_kurt):.4f}')
 
-    # Autocorrelation
-    plt.plot(jnp.arange(max_lags), jnp.mean(autocorrs, axis=0), label=method)
-
-plt.legend()
-plt.show()
