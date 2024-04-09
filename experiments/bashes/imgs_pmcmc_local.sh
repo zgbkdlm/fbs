13e950ae405ec325a6353d6724d30a397f174f02
Zheng Zhao <zgbkdlm@gmail.com>
2024-04-09 14:45:17 +0200

    sr struggle

--- experiments/bashes/imgs_pmcmc_local.sh
+++ experiments/bashes/imgs_pmcmc_local.sh
@@ -9,17 +9,20 @@ nparticles=$2
 if [[ "$dataset" == "mnist" ]]
 then
     rect_size=15
+    sr_rate=4
 elif [[ "$dataset" == "celeba-64" ]]
 then
     rect_size=32
+    sr_rate=2
 elif [[ "$dataset" == "celeba-128" ]]
 then
     rect_size=64
+    sr_rate=2
 else
     echo "Invalid dataset"
     exit 1
 fi
 
 python imgs/inpainting.py --dataset="$dataset" --rect_size=$rect_size --sde="lin" --method="pmcmc-0.005" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
-python imgs/supr.py --dataset="$dataset" --rate=2 --sde="lin" --method="pmcmc-0.005" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
+python imgs/supr.py --dataset="$dataset" --rate=$sr_rate --sde="lin" --method="pmcmc-0.005" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
 wait
--- experiments/bashes/imgs_twisted.sh
+++ experiments/bashes/imgs_twisted.sh
@@ -19,17 +19,20 @@ nparticles=$2
 if [[ "$dataset" == "mnist" ]]
 then
     rect_size=15
+    sr_rate=4
 elif [[ "$dataset" == "celeba-64" ]]
 then
     rect_size=32
+    sr_rate=2
 elif [[ "$dataset" == "celeba-128" ]]
 then
     rect_size=64
+    sr_rate=2
 else
     echo "Invalid dataset"
     exit 1
 fi
 
 python imgs/inpainting_twisted.py --dataset="$dataset" --rect_size=$rect_size --sde="lin" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
-python imgs/supr_twisted.py --dataset="$dataset" --rate=2 --sde="lin" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
+python imgs/supr_twisted.py --dataset="$dataset" --rate=$sr_rate --sde="lin" --test_nsteps=1000 --test_epoch=2999 --test_ema --test_seed=996 --ny0s=10 --nparticles=$nparticles --nsamples=100 &
 wait
