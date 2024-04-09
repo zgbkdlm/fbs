857fdf8a797d36a503489a23a896fba22e802b01
Zheng Zhao <zgbkdlm@gmail.com>
2024-04-09 15:22:19 +0200

    add dardel compute

--- experiments/run_toy_all.sh
+++ experiments/run_toy_all.sh
@@ -2,9 +2,19 @@
 
 nparticles=$1
 sde=$2
+cluster=$3
 
-sbatch bashes/toy_filter.sh $nparticles $sde
-sbatch bashes/toy_gibbs.sh $nparticles $sde
-sbatch bashes/toy_pmcmc.sh $nparticles $sde 0.005
-sbatch bashes/toy_twisted.sh $nparticles $sde
-sbatch bashes/toy_csgm.sh $sde
+if [[ "$cluster" == "dardel" ]]
+then
+    sbatch bashes/toy_filter_dardel.sh $nparticles $sde
+    sbatch bashes/toy_gibbs_dardel.sh $nparticles $sde
+    sbatch bashes/toy_pmcmc_dardel.sh $nparticles $sde 0.005
+    sbatch bashes/toy_twisted_dardel.sh $nparticles $sde
+    sbatch bashes/toy_csgm_dardel.sh $sde
+else
+    sbatch bashes/toy_filter.sh $nparticles $sde
+    sbatch bashes/toy_gibbs.sh $nparticles $sde
+    sbatch bashes/toy_pmcmc.sh $nparticles $sde 0.005
+    sbatch bashes/toy_twisted.sh $nparticles $sde
+    sbatch bashes/toy_csgm.sh $sde
+fi
