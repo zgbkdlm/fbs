3d40508f9a8748d1ff92316429a18be74c01e650
Zheng Zhao <zgbkdlm@gmail.com>
2024-04-25 12:59:51 +0200

    finalise plots

--- experiments/tabulators/plot_imgs.py
+++ experiments/tabulators/plot_imgs.py
@@ -1,8 +1,24 @@
+"""
+Plot the MNIST and Celeba images in the paper main body.
+
+Layout being like:
+            ===============(inpainting)========================= | ======(super-resolution)================
+method |    corrupt img | true img | sample0 | sample1 | sample2 | ...
+...
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 from fbs.data.images import normalise
 from mpl_toolkits.axes_grid1 import ImageGrid
 from PIL import Image
+
+np.random.seed(666)
+
+plt.rcParams.update({
+    'text.usetex': True,
+    'font.family': "serif",
+    'text.latex.preamble': r'\usepackage{amsmath,amsfonts}',
+    'font.size': 14})
 
 
 def to_img(img):
@@ -15,38 +31,50 @@ task = 'supr-2'
 rnd_mask = False
 sde = 'lin'
 nparticles = 10
-ny0s = 20
+y0_id = 19
 nsamples = 100
+methods = ['filter', 'gibbs-eb-ef', 'pmcmc-0.005', 'twisted', 'csgm']
+nexamples = 3
+
+sample_inds = np.random.choice(np.arange(nsamples), nexamples, replace=False)
 
 img_hw = (28, 28) if dataset == 'mnist' else (64, 64)
+fig = plt.figure(figsize=(5, 5.5))
+grid = ImageGrid(fig, 111, nrows_ncols=(len(methods), nexamples + 2), axes_pad=0.)
 
-for method in ['filter', 'gibbs-eb-ef', 'pmcmc-0.005', 'twisted', 'csgm']:
-    np.random.seed(666)
-    path_head = f'./imgs/results_{task.split("-")[0]}/imgs/{dataset}-{task.split("-")[1]}'
-    path_head = path_head + '-rm' if 'supr' in task and rnd_mask else path_head
-    path_head = path_head + f'-{sde}-'
+path_head = f'./imgs/results_{task.split("-")[0]}/imgs/{dataset}-{task.split("-")[1]}'
+path_head = path_head + '-rm' if 'supr' in task and rnd_mask else path_head
+path_head = path_head + f'-{sde}-'
+filename = path_head + f'{y0_id}-{"corrupt-lr" if "supr" in task else "corrupt"}.png'
+img_corrupt = np.asarray(Image.open(filename).resize(img_hw, resample=Image.Resampling.NEAREST))
+filename = path_head + f'{y0_id}-true.png'
+img_true = np.asarray(Image.open(filename))
 
-    fig = plt.figure(figsize=(5, 5))
-    grid = ImageGrid(fig, 111, nrows_ncols=(4, 4), axes_pad=0.)
-    y0_inds = np.random.choice(np.arange(ny0s), 4, replace=False)
-
-    for row in range(4):
-        filename = path_head + f'{y0_inds[row]}-{"corrupt-lr" if "supr" in task else "corrupt"}.png'
-        img_corrupt = np.asarray(Image.open(filename).resize(img_hw, resample=Image.Resampling.NEAREST))
-        for col in range(4):
-            idx = row * 4 + col
-            if col == 0:
-                grid[idx].imshow(img_corrupt)
+for row in range(len(methods)):
+    for col in range(nexamples + 2):
+        axes_idx = row * (nexamples + 2) + col
+        if col == 0:
+            grid[axes_idx].imshow(img_corrupt)
+        elif col == 1:
+            grid[axes_idx].imshow(img_true)
+        else:
+            method = methods[row]
+            if method == 'csgm':
+                filename = path_head + f'{y0_id}-{method}-{sample_inds[col - 2]}.png'
             else:
-                if method == 'csgm':
-                    filename = path_head + f'{y0_inds[row]}-{method}-{col * 30}.png'
-                else:
-                    filename = path_head + f'{nparticles}-{y0_inds[row]}-{method}-{col * 30}.png'
-                img_restored = np.asarray(Image.open(filename))
-                grid[idx].imshow(img_restored)
+                filename = path_head + f'{nparticles}-{y0_id}-{method}-{sample_inds[col - 2]}.png'
+            img_restored = np.asarray(Image.open(filename))
+            grid[axes_idx].imshow(img_restored)
 
-            grid[idx].axis('off')
+        if row == 0 and col == 0:
+            grid[axes_idx].set_title('corrupt')
+        elif row == 0 and col == 1:
+            grid[axes_idx].set_title('true')
+        elif row == 0 and col > 1:
+            grid[axes_idx].set_title(f'sample {col - 2}')
 
-    plt.tight_layout(pad=0.1)
-    plt.savefig(f'./tmp_figs/imgs-{dataset}-{task}-{method}-{nparticles}.png', transparent=True)
-    plt.show()
+        grid[axes_idx].axis('off')
+
+plt.tight_layout(pad=0.1)
+plt.savefig(f'./tmp_figs/imgs-{dataset}-{task}-{nparticles}-{y0_id}.png', transparent=True)
+plt.show()
