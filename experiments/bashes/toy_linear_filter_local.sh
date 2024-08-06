807c48c360c9c613945b6066ea97c8c837bef787
Zheng Zhao <zgbkdlm@gmail.com>
2024-08-06 12:03:10 +0200

    prepare to run

--- experiments/bashes/toy_linear_filter_local.sh
+++ experiments/bashes/toy_linear_filter_local.sh
@@ -5,11 +5,12 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.01
 
 nparticles=$1
 
-SEQUENTIAL_MAX=10
+NRUNS=10
 
-for (( i=0;i<SEQUENTIAL_MAX;i++ ))
+for (( i=0;i<NRUNS;i++ ))
 do
     for obs_var in 0.01 0.1 1 10; do
-        python toy/gp_linear_filter.py --id=$i --d=100 --obs_var=$obs_var --nsamples=10000 --nparticles=$nparticles
+        python toy/gp_linear_filter.py --id=$i --d=100 --obs_var=$obs_var --nsamples=10000 --nparticles=$nparticles &
     done
+    wait
 done
--- experiments/bashes/toy_linear_gibbs_local.sh
+++ experiments/bashes/toy_linear_gibbs_local.sh
@@ -5,11 +5,12 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.02
 
 nparticles=$1
 
-SEQUENTIAL_MAX=10
+NRUNS=10
 
-for (( i=0;i<SEQUENTIAL_MAX;i++ ))
+for (( i=0;i<NRUNS;i++ ))
 do
     for obs_var in 0.01 0.1 1 10; do
-        python toy/gp_linear_gibbs.py --id=$i --d=100 --obs_var=$obs_var --nsamples=10000 --nparticles=$nparticles --explicit_backward
+        python toy/gp_linear_gibbs.py --id=$i --d=100 --obs_var=$obs_var --nsamples=10000 --nparticles=$nparticles --explicit_backward &
     done
+    wait
 done
