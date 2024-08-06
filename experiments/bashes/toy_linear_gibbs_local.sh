ea5af097c5fd02737ea32bd04a47db98fd2234b2
Zheng Zhao <zgbkdlm@gmail.com>
2024-08-06 13:48:41 +0200

    change bash

--- experiments/bashes/toy_linear_gibbs_local.sh
+++ experiments/bashes/toy_linear_gibbs_local.sh
@@ -5,11 +5,10 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.02
 
 nparticles=$1
 
-NRUNS=10
+PRUNS=10
 
-for (( i=0;i<NRUNS;i++ ))
-do
-    for obs_var in 0.01 0.1 1 10; do
+for obs_var in 0.01 0.1 1 10; do
+    for (( i=0;i<PRUNS;i++ )); do
         python toy/gp_linear_gibbs.py --id=$i --d=100 --obs_var=$obs_var --nsamples=10000 --nparticles=$nparticles --explicit_backward &
     done
     wait
