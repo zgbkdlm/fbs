e3d4fc1cd9aa2bb832d05e247d322dcb495643b8
zgbkdlm <zgbkdlm@gmail.com>
2024-08-10 10:30:51 +0200

    run gibbs mh

--- experiments/bashes/sb_imgs_mh.sh
+++ experiments/bashes/sb_imgs_mh.sh
@@ -7,6 +7,6 @@ nparticles=$1
 
 for (( i=0;i<100;i++ ))
 do
-    python sb_imgs/supr.py --sb_step=9 --method='gibbs' --y0_id=$i --test_nsteps=64 --nsamples=100 --nparticles=$nparticles
+    python sb_imgs/supr_with_acc.py --sb_step=9 --method='gibbs' --y0_id=$i --test_nsteps=64 --nsamples=100 --nparticles=$nparticles
     wait
 done
