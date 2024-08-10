3161dd26322c6b97efcf9593d9a3603c9e49bdcc
zgbkdlm <zgbkdlm@gmail.com>
2024-08-10 11:04:17 +0200

    run gibbs mh

--- experiments/bashes/sb_imgs_mh.sh
+++ experiments/bashes/sb_imgs_mh.sh
@@ -4,9 +4,13 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.40
 
 nparticles=$1
-use_mh=${2:-1}
+use_mh=$2
 
 for (( i=0;i<100;i++ ))
 do
-    python sb_imgs/supr_with_acc.py --sb_step=9 --y0_id=$i --test_nsteps=128 --nsamples=100 --nparticles=$nparticles --use_mh=$use_mh
+    if $use_mh; then
+      python sb_imgs/supr_with_acc.py --sb_step=9 --y0_id=$i --test_nsteps=128 --nsamples=100 --nparticles=$nparticles --use_mh
+    else
+      python sb_imgs/supr_with_acc.py --sb_step=9 --y0_id=$i --test_nsteps=128 --nsamples=100 --nparticles=$nparticles
+    fi
 done
